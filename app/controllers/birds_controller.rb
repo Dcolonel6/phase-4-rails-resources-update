c50ec320547a185dc00e9a4f692f232fbf055517
Dennis Njogu <dennis.njogu@student.moringaschool.com>
2022-11-26 13:14:28 +0300

    finished assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,35 @@ class BirdsController < ApplicationController
     end
   end
 
+  #PATCH, PUT /birds/:id
+
+  def update
+    #find bird
+    bird = Bird.find_by(id:params[:id])
+    #check if we have a bird
+    if bird
+      #send updated bird
+      bird.update(bird_params)
+      render json: bird, status: :accepted
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
